d4c90f9399da177439159760cfdcdccc7877c3c1
YSH <98166742+yshhhh-cn@users.noreply.github.com>
2024-05-16 16:35:08 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,77 +1,20 @@
 # -*- coding:utf-8 -*-
-"""
-
-Author:
-    Weichen Shen,weichenswc@163.com
-
-"""
 import numpy as np
 import torch
 import os
 
 
-def concat_fun(inputs, axis=-1):
-    if len(inputs) == 1:
-        return inputs[0]
-    else:
-        return torch.cat(inputs, dim=axis)
-
-
-def slice_arrays(arrays, start=None, stop=None):
-    """Slice an array or list of arrays.
-
-    This takes an array-like, or a list of
-    array-likes, and outputs:
-        - arrays[start:stop] if `arrays` is an array-like
-        - [x[start:stop] for x in arrays] if `arrays` is a list
-
-    Can also work on list/array of indices: `slice_arrays(x, indices)`
-
-    Arguments:
-        arrays: Single array or list of arrays.
-        start: can be an integer index (start index)
-            or a list/array of indices
-        stop: integer (stop index); should be None if
-            `start` was a list.
+def save_model(model, model_dir, epoch=None, val_loss=None):
+    """
+    Args:
+        model: model var.
+        model_dir: save path.
+        epoch: save model file name.
+        val_loss: save model file name.
 
     Returns:
-        A slice of the array(s).
-
-    Raises:
-        ValueError: If the value of start is a list and stop is not None.
+        return model save path.
     """
-
-    if arrays is None:
-        return [None]
-
-    if isinstance(arrays, np.ndarray):
-        arrays = [arrays]
-
-    if isinstance(start, list) and stop is not None:
-        raise ValueError('The stop argument has to be None if the value of start '
-                         'is a list.')
-    elif isinstance(arrays, list):
-        if hasattr(start, '__len__'):
-            # hdf5 datasets only support list objects as indices
-            if hasattr(start, 'shape'):
-                start = start.tolist()
-            return [None if x is None else x[start] for x in arrays]
-        else:
-            if len(arrays) == 1:
-                return arrays[0][start:stop]
-            return [None if x is None else x[start:stop] for x in arrays]
-    else:
-        if hasattr(start, '__len__'):
-            if hasattr(start, 'shape'):
-                start = start.tolist()
-            return arrays[start]
-        elif hasattr(start, '__getitem__'):
-            return arrays[start:stop]
-        else:
-            return [None]
-
-
-def save_model(model, model_dir, epoch=None, val_loss=None):
     if model_dir is None:
         return
     if not os.path.exists(model_dir):
@@ -86,95 +29,49 @@ def save_model(model, model_dir, epoch=None, val_loss=None):
     return file_name
 
 
-def load_model(model_dir, epoch=None):
-    if not model_dir:
-        return
-    epoch = str(epoch) if epoch is not None else 'best'
-    file_name = os.path.join(model_dir, epoch + '_dhfm.pt')
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-    if not os.path.exists(file_name):
-        return
-    with open(file_name, 'rb') as f:
-        model = torch.load(f)
-    return model
-
-
 def MAPE(v, v_, axis=None):
-    '''
+    """
     Mean absolute percentage error.
     :param v: np.ndarray or int, ground truth.
     :param v_: np.ndarray or int, prediction.
     :param axis: axis to do calculation.
     :return: int, MAPE averages on all elements of input.
-    '''
+    """
     mape = (np.abs(v_ - v) / (np.abs(v) + 1e-5)).astype(np.float64)
     mape = np.where(mape > 5, 5, mape)
     return np.mean(mape, axis)
 
 
 def RMSE(v, v_, axis=None):
-    '''
+    """
     Mean squared error.
     :param v: np.ndarray or int, ground truth.
     :param v_: np.ndarray or int, prediction.
     :param axis: axis to do calculation.
     :return: int, RMSE averages on all elements of input.
-    '''
+    """
     return np.sqrt(np.mean((v_ - v) ** 2, axis)).astype(np.float64)
 
 
 def MAE(v, v_, axis=None):
-    '''
+    """
     Mean absolute error.
     :param v: np.ndarray or int, ground truth.
     :param v_: np.ndarray or int, prediction.
     :param axis: axis to do calculation.
     :return: int, MAE averages on all elements of input.
-    '''
+    """
     return np.mean(np.abs(v_ - v), axis).astype(np.float64)
 
 
-def masked_mse(preds, labels, null_val=np.nan):
-    if np.isnan(null_val):
-        mask = ~torch.isnan(labels)
-    else:
-        mask = (labels != null_val)
-    mask = mask.float()
-    mask /= torch.mean(mask)
-    mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
-    loss = (preds - labels) ** 2
-    loss = loss * mask
-    loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
-    return torch.mean(loss)
-
-
-def masked_mae(preds, labels, null_val=np.nan):
-    if np.isnan(null_val):
-        mask = ~torch.isnan(labels)
-    else:
-        mask = (labels != null_val)
-    mask = mask.float()
-    mask /= torch.mean(mask)
-    mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
-    loss = torch.abs(preds - labels)
-    loss = loss * mask
-    loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
-    return torch.mean(loss)
-
-
-def loss(preds, labels, null_val=np.nan):
-    return (masked_mae(preds, labels, null_val) + masked_mse(preds, labels, null_val)) / 2
-
-
 def evaluate(y, y_hat, by_step=False, by_node=False):
-    '''
+    """
     :param y: array in shape of [count, time_step, node].
     :param y_hat: in same shape with y.
     :param by_step: evaluate by time_step dim.
     :param by_node: evaluate by node dim.
     :return: array of mape, mae and rmse.
-    '''
+    """
     if not by_step and not by_node:
         return MAPE(y, y_hat), MAE(y, y_hat), RMSE(y, y_hat)
     if by_step and by_node:
